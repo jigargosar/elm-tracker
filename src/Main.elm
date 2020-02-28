d69abe553aea4090e1d061cc82945f0d529cee18
Jigar Gosar <jigar.gosar@gmail.com>
2020-02-28 20:33:28 +0530

    modeling projects

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,21 @@
 module Main exposing (main)
 
 import Html exposing (text)
+import Random exposing (Generator)
+
+
+type alias Project =
+    { id : ProjectId
+    }
+
+
+type ProjectId
+    = ProjectId String
+
+
+idGen : (String -> id) -> Generator id
+idGen tag =
+    Random.int 0 Random.maxInt |> Random.map (String.fromInt >> tag)
 
 
 main =
