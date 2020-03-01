9a2297bf7f96ee09e38aae72e0454e3a67b38527
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-01 11:57:27 +0530

    try automating elm install

--- scripts/auto.js
+++ scripts/auto.js
@@ -13,7 +13,13 @@ const execa = require('execa')
     if (DEBUG) {
       console.error('ERROR: INSPECTING', inspect(e, false, 0, true))
     } else {
-      console.error('ERROR: ', e.message)
+      console.error('ERROR: ', e.message, e.code)
     }
   }
+
+  await execa('elm', ['install', 'elm/core'], {
+    input: 'Y\n',
+    stdout: 'inherit',
+    stderr: 'inherit',
+  })
 })()
