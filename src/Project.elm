8f445b7f7c608d14770ff1b6bf92a3e17ac4f694
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-03 21:03:16 +0530

    decode LogDict.elm

--- src/Project.elm
+++ src/Project.elm
@@ -1,5 +1,7 @@
-module Project exposing (Project, generator, id, idString, title)
+module Project exposing (Project, decoder, encoder, generator, id, idString, title)
 
+import Json.Decode as JD exposing (Decoder)
+import Json.Encode as JE exposing (Value)
 import ProjectId exposing (ProjectId)
 import Random exposing (Generator)
 
@@ -8,6 +10,21 @@ type alias Project =
     { id_ : ProjectId
     , title_ : String
     }
+
+
+decoder : Decoder Project
+decoder =
+    JD.map2 Project
+        (JD.field "id_" ProjectId.decoder)
+        (JD.field "title_" JD.string)
+
+
+encoder : Project -> Value
+encoder project =
+    JE.object
+        [ ( "id_", ProjectId.encoder project.id_ )
+        , ( "title_", JE.string project.title_ )
+        ]
 
 
 generator : String -> Generator Project
