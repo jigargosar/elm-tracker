0325f375f49cacc1fcbe843d903271ff8c19e60d
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-03 17:10:31 +0530

    nav links

--- src/Main.elm
+++ src/Main.elm
@@ -393,10 +393,18 @@ viewTimeLine today zone pd logs =
                     [ text "|>" ]
                 ]
 
+        dateToString : Date -> String
+        dateToString date =
+            if date == today then
+                "Today"
+
+            else
+                Date.format "E ddd MMM y" date
+
         viewDateGroup : ( Date, List ( ProjectId, TypedTime ) ) -> Html Msg
         viewDateGroup ( date, projectEntryList ) =
             column [ class "pv2" ]
-                (row [ class "f4" ] [ text (Date.format "E ddd MMM y" date) ]
+                (row [ class "f4" ] [ text (dateToString date) ]
                     :: List.map viewProjectEntry projectEntryList
                 )
 
@@ -459,7 +467,7 @@ viewTracked vm =
                 |> TypedTime.toString TypedTime.Seconds
     in
     column [ class "pv2" ]
-        [ row [ class "pv1 f4 " ] [ text "Tracking Today" ]
+        [ row [ class "pv1 f4 " ] [ text "Tracking" ]
         , row [ class "mb1 justify-between items-baseline" ]
             [ row [] [ text vm.title ]
             , row [] [ text elapsed ]
